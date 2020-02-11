d33085e49afb4b4173cf317c5c234b749852f9d1
triard <triard78@gmail.com>
2020-02-12 03:54:04 +0700

    final constraint

---
+++ ConstraintLayout/app/src/main/java/id/ac/polinema/constraintlayout/Main2Activity.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
